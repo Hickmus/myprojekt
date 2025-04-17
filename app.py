fddcaea98b98b79ad7c3a7e072634522fb29529a
Hickmus <logoskypvpare10ttt@gmail.com>
2025-04-17 18:01:50 +0200

    adding if projekt

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ x = random.randint(1, 100)
 y = random.randint(1, 100)
 print(x)
 print(y)
+# Compare this snippet from style.py:
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is less than y")
+else:
+    print("x is equal to y")
